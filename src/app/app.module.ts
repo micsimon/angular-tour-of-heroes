9e36282964a25dc15b6fa4d31abe7e0a54301fd6
Michael Simon <5222293+micsimon@users.noreply.github.com>
2022-08-06 21:32:03 +0200

    5. add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './_app/app.component';
-import { HeroesComponent } from './component/heroes/heroes.component';
+import {AppComponent} from './_app/app.component';
+import {HeroesComponent} from './component/heroes/heroes.component';
 import {FormsModule} from "@angular/forms";
-import { HeroDetailComponent } from './component/hero-detail/hero-detail.component';
-import { MessagesComponent } from './component/messages/messages.component';
+import {HeroDetailComponent} from './component/hero-detail/hero-detail.component';
+import {MessagesComponent} from './component/messages/messages.component';
+import {AppRoutingModule} from './app-routing.module';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/hero-detail/hero-detail.component.ts
+++ src/app/component/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../../service/hero/hero.service";
 import {Hero} from "../../../model/hero";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,11 +10,23 @@ import {Hero} from "../../../model/hero";
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
 
-  constructor() { }
+  hero?: Hero;
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) {
   }
 
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  private getHero() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
